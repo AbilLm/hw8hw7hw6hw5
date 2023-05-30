5e940c36c500417f519ff62e6762d1abb579a985
GigaN1ga <loppupossum@gmail.com>
2023-05-30 17:04:55 +0600

    hw6

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ from aiogram import Dispatcher, Bot
 from dotenv import load_dotenv
 import os
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+scheduler = AsyncIOScheduler()
 
 storage = MemoryStorage()
 load_dotenv()
--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from handlers.picture import pic
 from handlers.start import start
 from handlers import fsm_anketa
 from db import bot_db
+from config import scheduler
+from handlers.notific import handle_scheduler
+
+scheduler.start()
 
 
 
@@ -11,6 +15,7 @@ bot_db.register_handlers_db(dp)
 
 if __name__ == "__main__":
     dp.register_message_handler(pic, commands=['picture'])
+    dp.register_message_handler(handle_scheduler, commands=['sche'])
     dp.register_message_handler(start, commands=['start'])
     fsm_anketa.register_fsm_handlers(dp)
     executor.start_polling(dp, skip_updates=True)
